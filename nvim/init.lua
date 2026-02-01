0e9b5d71d3b916359fb9160f50648764a671ca65
13cronil <lukecronin1228@gmail.com>
2026-02-01 16:30:07 +0000

    init commit

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+require '13cronil.core.options'
+require '13cronil.core.keymaps'
+require '13cronil.core.autocmds'
+require '13cronil.lazy'
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
