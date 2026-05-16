fb5ed4f3fff2346213cad0afcc6c3dfb3a10a5df
Luke Cronin <lukejpcronin@gmail.com>
2026-05-16 13:29:26 +0100

    update oil config

--- nvim/lua/13cronil/core/plugins/oil.lua
+++ nvim/lua/13cronil/core/plugins/oil.lua
@@ -5,18 +5,23 @@ return {
   config = function()
     require("oil").setup {
       default_file_explorer = true,
+      columns = { "icon" },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<M-h>"] = "actions.select.split",
+      },
       delete_to_trash = true,
       skip_confirm_for_simple_edits = true,
       view_options = {
         show_hidden = true,
         natural_order = true,
-        is_always_hidden = function(name, _)
-          return name == ".." or name == ".git"
-        end,
-      },
-      win_options = {
-        wrap = true,
+        win_options = { wrap = true },
       },
     }
+    -- Open parent directory in current window
+    vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+
+    -- Open parent directory in floating window
+    vim.keymap.set("n", "<space>--", require("oil").toggle_float)
   end,
 }
